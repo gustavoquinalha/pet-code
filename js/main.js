3abd20cb3d12f251c78f25174ff2c3e7124ab6fe
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-03-13 12:25:24 -0300

    -q

--- js/main.js
+++ js/main.js
@@ -6,18 +6,15 @@ var qrcode = new QRCode(document.getElementById("qrcode"), {
 
 function makeCode () {
   var elText = document.getElementById("text");
-
   if (!elText.value) {
-    //alert("Digite um texto");
+    alert("Digite um texto");
     elText.focus();
     return;
   }
 
   qrcode.makeCode(elText.value);
   //alert("criado");
-
   //event.preventDefault();
-
   //Convertendo svg em canvas
   var canvas = document.getElementById("canvas");
   var svg = $("#svg").html().replace(/>\s+/g, ">").replace(/\s+</g, "<").replace(" xlink=", " xmlns:xlink=").replace(/\shref=/g, " xlink:href=");
@@ -32,14 +29,16 @@ function makeCode () {
 
 makeCode();
 
-$( "#tey" ).click(function() {
+$("#tey").click(function() {
   makeCode();
 });
 
-$("#text").
-on("blur", function () {
+
+
+$("#text").on("blur", function () {
   makeCode();
 }).
+
 on("keydown", function (e) {
   if (e.keyCode == 13) {
     makeCode();
